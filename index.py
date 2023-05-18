2e2c8342d4cf58e2a8ade90a7ac622abf77ae0a5
preeti_dev <preetish@zeta.tech>
2023-05-18 15:30:32 +0530

    changes done key

--- index.py
+++ index.py
@@ -22,8 +22,9 @@ def save_images(img_url):
     img=Image.open(f"{img_url}.png")
     img.save(f"images/{img_url}.png")
     
-print((base64.b64decode("c2stMHhHZmJPenVvQ1N1aTFmbnZXVnhUM0JsYmtGSmVwZm9WSFVzNTVFRkdOQU9xbUcz").decode('ascii')))
 openai.api_key=base64.b64decode("c2stMHhHZmJPenVvQ1N1aTFmbnZXVnhUM0JsYmtGSmVwZm9WSFVzNTVFRkdOQU9xbUcz").decode('ascii')
+
+
 
 
 def correct_sent(sent):
